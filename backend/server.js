4c33b5e52b2edaa57f26f8456ed7752238e32b81
XobliteHD <xoblitehd@gmail.com>
2025-05-04 00:53:28 +0100

    Fixed app.use(cors...

--- backend/server.js
+++ backend/server.js
@@ -50,25 +50,12 @@ app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        console.log(`CORS: Allowed origin: ${origin || "N/A"}`);
         callback(null, true);
       } else {
         console.error(`CORS Error: Origin ${origin} not allowed.`);
         callback(new Error("Not allowed by CORS"));
       }
-    },
-  })
-);
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
     },
   })
 );
